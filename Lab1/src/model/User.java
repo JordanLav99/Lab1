f65b6a21131030057a739ebaf3d1bc09bb935eba
EmmettM785 <159438781+EmmettM785@users.noreply.github.com>
2024-02-08 12:07:44 +0000

    Add files via upload

---
+++ Lab1/src/model/User.java
@@ -0,0 +1,12 @@
+package model;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private String first_name;
+	private String last_name;
+	private String mobile_number;
+	
+
+}
